12577dd9d0cf88ebc80bf6303d06642039ccf8ef
AL1iX <133574202+AL1iX@users.noreply.github.com>
2025-10-22 00:00:25 +0500

    Add files via upload

---
+++ EmptyMazeTask.cs
@@ -0,0 +1,24 @@
+﻿namespace Mazes;
+
+public static class EmptyMazeTask
+{
+	public static void MoveOut(Robot robot, int width, int height)
+	{
+		HorizontalMove(robot, Direction.Right, width);
+		VerticalMove(robot, Direction.Down, height);
+	}
+
+	public static void HorizontalMove(Robot robot, Direction direction, int width)
+	{
+		var horizontalDist = width - 2 - robot.X;
+		for (int i = 0; i < horizontalDist; i++)
+			robot.MoveTo(direction);
+	}
+	 
+	public static void VerticalMove(Robot robot, Direction direction, int height)
+	{
+		var verticalDist = height - 2 - robot.Y;
+		for (int i = 0; i < verticalDist; i++) 
+			robot.MoveTo(direction);
+	}
+}
