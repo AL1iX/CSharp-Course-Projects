a9a6bc5d2eaec0e1c477e76c17697c98e4edaefc
AL1iX <133574202+AL1iX@users.noreply.github.com>
2025-10-29 00:34:46 +0500

    Add files via upload

---
+++ PluralizeTask.cs
@@ -0,0 +1,17 @@
+﻿namespace Pluralize;
+
+public static class PluralizeTask
+{
+	public static string PluralizeRubles(int count)
+	{
+        int lastDigit = count % 10;
+        bool between11And20 = (count % 100 >= 11 && count % 100 < 20);
+
+        if (between11And20 || lastDigit == 0 || lastDigit >= 5)
+            return "рублей";
+        else if (lastDigit >= 2 && lastDigit <= 4)
+            return "рубля";
+        else
+            return "рубль";
+    }
+}
