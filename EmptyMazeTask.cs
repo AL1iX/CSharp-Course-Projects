7d0aae8f278ed239313445fb758cc0d94c9de1eb
AL1iX <133574202+AL1iX@users.noreply.github.com>
2025-11-06 18:17:19 +0500

    Update EmptyMazeTask.cs

--- EmptyMazeTask.cs
+++ EmptyMazeTask.cs
@@ -1,24 +1,16 @@
-﻿namespace Mazes;
+namespace Mazes;
 
 public static class EmptyMazeTask
 {
-	public static void MoveOut(Robot robot, int width, int height)
-	{
-		HorizontalMove(robot, Direction.Right, width);
-		VerticalMove(robot, Direction.Down, height);
-	}
+    public static void MoveOut(Robot robot, int width, int height)
+    {
+        DirectionMove(robot, Direction.Right, width - 2 - robot.X);
+        DirectionMove(robot, Direction.Down, height - 2 - robot.Y);
+    }
 
-	public static void HorizontalMove(Robot robot, Direction direction, int width)
-	{
-		var horizontalDist = width - 2 - robot.X;
-		for (int i = 0; i < horizontalDist; i++)
-			robot.MoveTo(direction);
-	}
-	 
-	public static void VerticalMove(Robot robot, Direction direction, int height)
-	{
-		var verticalDist = height - 2 - robot.Y;
-		for (int i = 0; i < verticalDist; i++) 
-			robot.MoveTo(direction);
-	}
+    public static void DirectionMove(Robot robot, Direction direction, int dist)
+    {
+        for (var i = 0; i < dist; i++)
+            robot.MoveTo(direction);
+    }
 }
